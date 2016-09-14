82b707a01f10f8a92036e429e214750682c00664
alexbagirov <alexfox@protonmail.ch>
2016-09-14 20:47:37 +0500

    Switches message format to JSON and some code tweaks, closes #3

--- bot.py
+++ bot.py
@@ -1,9 +1,27 @@
 from urllib.request import urlopen
+from urllib.error import URLError
 from datetime import datetime
-#from slacker import Slacker
+from slacker import Slacker
+from jsonifier import TableJSONifier
 import logging, os, re
 
-def main():
+class TimeTableBot:
+    months = {'ЯНВАРЯ':    1,
+              'ФЕВРАЛЯ':   2,
+              'МАРТА':     3,
+              'АПРЕЛЯ':    4,
+              'МАЯ':       5,
+              'ИЮНЯ':      6,
+              'ИЮЛЯ':      7,
+              'АВГУСТА':   8,
+              'СЕНТЯБРЯ':  9,
+              'ОКТЯБРЯ':  10,
+              'НОЯБРЯ':   11,
+              'ДЕКАБРЯ':  12}
+
+    curr_date = datetime.now()
+
+    # Set up logging
     log_level = logging.DEBUG
 
     log = logging.Logger('bot')
@@ -18,89 +36,110 @@ def main():
 
     log.addHandler(log_handler)
 
-    log.info('starting up')
 
-    months = {'ЯНВАРЯ':   1,
-              'ФЕВРАЛЯ':  2,
-              'МАРТА':    3,
-              'АПРЕЛЯ':   4,
-              'МАЯ':      5,
-              'ИЮНЯ':     6,
-              'ИЮЛЯ':     7,
-              'АВГУСТА':  8,
-              'СЕНТЯБРЯ': 9,
-              'ОКТЯБРЯ': 10,
-              'НОЯБРЯ':  11,
-              'ДЕКАБРЯ': 12}
+    def already_sent(self):
+        """Checks if the changes were sent already"""
+        with open('changes.txt') as f:
+            tm = int(f.read())
+            self.changes_sent = datetime.fromtimestamp(tm)
+            if self.changes_sent > self.curr_date:
+                return True
+            return False
 
-    curr_date = datetime.now()
+    def get_table_page(self, addr, enc):
+        """Gets the changes page's content"""
+        self.log.info('fetching table page')
+        page = urlopen(addr).read().decode(enc)
+        self.log.info('fetch successful')
+        return page
 
-    with open('changes.txt') as f:
-        tm = int(f.read())
-        changes_sent = datetime.fromtimestamp(tm)
-        if changes_sent > curr_date:
-            log.info('already up-to-date, quitting')
-            log.debug('latest update sent for ' +
-                      changes_sent.strftime('%d-%m-%Y'))
+    def parse_header(self, page):
+        """Dissects the heading in the changes page"""
+        header = re.search('ИЗМЕНЕНИЯ В РАСПИСАНИИ НА ([А-Я]+), ([0-9]+) ([А-Я]+)', page)
+        try:
+            self.wkday, self.day, self.month = header.group(1, 2, 3)
+            return True
+        except AttributeError:
+            return False
+
+    def parse_changes(self, page):
+        """Returns a list of changes for both classes
+        (if they're present)"""
+        changes_list = []
+        changes10_match = re.search('<h2>10Е</h2>\s+?<p>([^<]+)', page)
+        changes11_match = re.search('<h2>11Е</h2>\s+?<p>([^<]+)', page)
+
+        if changes10_match:
+            changes_list.append(('10Е', changes10_match.group(1).replace('&nbsp;&mdash;', ' -')))
+            self.log.info('update for 10 found')
+            self.log.debug('update:\n' + changes10_match.group(1))
+        else:
+            self.log.info('no updates for 10')
+
+        if changes11_match:
+            changes_list.append(('11E', changes11_match.group(1).replace('&nbsp;&mdash;', ' -')))
+            self.log.info('update for 11 found')
+            self.log.debug('update:\n' + changes11_match.group(1))
+        else:
+            self.log.info('no updates for 11')
+
+        return changes_list
+
+    def set_timestamp(self, change_date):
+        """Sets the new timestamp"""
+        with open('changes.txt', 'w') as f:
+            self.log.info('setting a new timestamp in changes.txt')
+            f.write(str(int(change_date.timestamp())))
+
+    def send(self, attachments):
+        """Sends a message to Slack"""
+        # Create a Slack bot instance
+        slack = Slacker(str(os.environ['SLACK_API_TOKEN']))
+
+        # Send a message to #general channel
+        self.log.info('sending a message to Slack')
+        slack.chat.post_message('#general',
+                                text = 'Timetable Updates',
+                                username = 'timetable',
+                                icon_emoji = ':spiral_calendar_pad:',
+                                attachments = attachments)
+
+    def run(self):
+        self.log.info('starting up')
+
+        if self.already_sent():
+            self.log.info('already up-to-date, quitting')
+            self.log.debug('latest update sent for ' +
+                           self.changes_sent.strftime('%d-%m-%Y'))
+
+        try:
+            self.page = self.get_table_page(os.environ['TIMETABLE_URL'],
+                                            os.environ['TIMETABLE_ENC'])
+        except URLError:
+            self.log.error('failed to fetch page, quitting')
+            return -1
+
+        if not self.parse_header(self.page):
+            self.log.error('table invalid, quitting')
+            return -1
+
+        self.change_date = datetime(self.curr_date.year,
+                                    self.months[self.month],
+                                    int(self.day))
+
+        if self.change_date < self.curr_date:
+            self.log.info('table outdated, quitting')
             return
 
-    log.info('fetching table page')
-    page = urlopen('https://moarcatz.github.io/slack-timetable/').read().decode()
-    log.info('fetch successful')
-    date_match = re.search('ИЗМЕНЕНИЯ В РАСПИСАНИИ НА ([А-Я]+), ([0-9]+) ([А-Я]+)', page)
-    try:
-        wkday, day, month = date_match.group(1, 2, 3)
-    except AttributeError:
-        log.error('table invalid, quitting')
-        pass
+        self.changes = self.parse_changes(self.page)
 
-    change_date = datetime(curr_date.year,
-                           months[month],
-                           int(day))
+        self.log.info('generating JSON message')
+        atch = TableJSONifier.make_attachment(self.wkday,
+                                              self.changes)
 
-    if change_date < curr_date:
-        log.info('table outdated, quitting')
-        return
+        self.send(atch)
+        self.set_timestamp(self.change_date)
+        self.log.info('success, quitting')
 
-    changes10_match = re.search('<h2>10Е</h2>\s+?<p>([^<]+)', page)
-    changes11_match = re.search('<h2>11Е</h2>\s+?<p>([^<]+)', page)
-
-    if changes10_match:
-        changes10 = changes10_match.group(1).replace('&nbsp;&mdash;', ' -')
-        log.info('update for 10 found')
-        log.debug('update:\n' + changes10_match.group(1))
-    else:
-        changes10 = ''
-        log.info('no updates for 10')
-
-    if changes11_match:
-        changes11 = changes11_match.group(1).replace('&nbsp;&mdash;', ' -')
-        changes11 = '*11Е*\n' + changes11
-        log.info('update for 11 found')
-        log.debug('update:\n' + changes11_match.group(1))
-    else:
-        changes11 = ''
-        log.info('no updates for 11')
-
-    log.info('generating a message to Slack')
-    message = (changes10 + '\n' + changes11 + '\n').strip()
-    log.debug('message:\n' + message)
-
-    with open('changes.txt', 'w') as f:
-        log.info('setting a new timestamp in changes.txt')
-        f.write(str(int(change_date.timestamp())))
-
-
-    # Create a Slack bot instance
-    slack = Slacker(str(os.environ['SLACK_API_TOKEN']))
-
-    # Send a message to #general channel
-    log.info('sending a message to Slack')
-    slack.chat.post_message('#general',
-                            'Привет! На сайте появились изменения в расписании:\n' + message,
-                            username = 'timetable',
-                            icon_emoji = ':spiral_calendar_pad:')
-
-    log.info('success, quitting')
-
-main()
+bot = TimeTableBot()
+bot.run()
---
+++ jsonifier.py
@@ -0,0 +1,28 @@
+import json
+
+class TableJSONifier:
+    @classmethod
+    def make_field(self, cls, changes):
+        return {"title": cls,
+                "value": changes,
+                "short": True}
+
+    @classmethod
+    def make_attachment(self, wkday, fields):
+        text = "На сайте появились изменения в расписании на {}!".format(wkday)
+        fld_list = [self.make_field(*fld) for fld in fields]
+
+        atch = {"fallback": "На сайте появились изменения в расписании!",
+                "color": "#36A64F",
+                "text": text,
+                "fields": fld_list}
+
+        return json.dumps({"attachments": [atch]},
+                          indent = 4,
+                          ensure_ascii = False)
+
+
+if __name__ == "__main__":
+    tj = TableJSONifier()
+    print(tj.make_attachment('понедельник', [('10Е', 'nothing'),
+                                             ('11E', 'nothing')]))
